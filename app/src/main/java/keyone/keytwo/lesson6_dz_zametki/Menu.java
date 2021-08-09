fd5942ff8bf174dc594ba5a55d258a482ce2f7bb
Natalia <nef.n@list.ru>
2021-08-09 17:43:45 +0300

    Заметки проверка 2

--- app/src/main/java/keyone/keytwo/lesson6_dz_zametki/Menu.java
+++ app/src/main/java/keyone/keytwo/lesson6_dz_zametki/Menu.java
@@ -3,56 +3,64 @@ package keyone.keytwo.lesson6_dz_zametki;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class Menu implements Parcelable {
-    protected Menu(Parcel in) {
-        imageIndex = in.readInt();
-        name = in.readString();
-    }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(imageIndex);
-        dest.writeString(name);
-    }
+    public class Menu implements Parcelable {
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+        protected Menu(int imageIndex, String name) {
+            this.imageIndex = imageIndex;
+            this.name = name;
+        }
 
-    public static final Creator<Menu> CREATOR = new Creator<Menu>() {
-        @Override
-        public Menu createFromParcel(Parcel in) {
-            return new Menu(in);
+
+        protected Menu(Parcel in) {
+            imageIndex = in.readInt();
+            name = in.readString();
         }
 
         @Override
-        public Menu[] newArray(int size) {
-            return new Menu[size];
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeInt(imageIndex);
+            dest.writeString(name);
         }
-    };
 
-    public int getImageIndex() {
-        return imageIndex;
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        public static final Creator<Menu> CREATOR = new Creator<Menu>() {
+            @Override
+            public Menu createFromParcel(Parcel in) {
+                return new Menu(in);
+            }
+
+            @Override
+            public Menu[] newArray(int size) {
+                return new Menu[size];
+            }
+        };
+
+        public int getImageIndex() {
+            return imageIndex;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private int imageIndex;
+        private String name;
+
+        public Menu(String name) {
+            this.name = name;
+        }
+
+
+        public void setImageIndex(int imageIndex) {
+            this.imageIndex = imageIndex;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
-
-    public String getName() {
-        return name;
-    }
-
-    private int imageIndex;
-    private String name;
-
-    public Menu(String name) {
-        this.name = name;
-    }
-
-
-    public void setImageIndex(int imageIndex) {
-        this.imageIndex = imageIndex;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
