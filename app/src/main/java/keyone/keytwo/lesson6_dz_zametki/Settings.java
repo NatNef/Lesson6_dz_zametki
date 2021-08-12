3fd7460f78b93c32dad3df36ae32bc65ed5ab34f
Natalia <nef.n@list.ru>
2021-08-12 22:49:48 +0300

    Дз 7 в процессе

--- app/src/main/java/keyone/keytwo/lesson6_dz_zametki/Settings.java
+++ app/src/main/java/keyone/keytwo/lesson6_dz_zametki/Settings.java
@@ -5,12 +5,14 @@ public class Settings {
     // создаем ключи
     public static final String SHARED_PREFERENCE_NAME = "SHARED_PREFERENCE_NAME";//ключ всех настроек
     public static final String IS_BACK_STACK_USED = "IS_BACK_STACK_USED";
+    public static final String IS_REPLACE_FRAGMENT = "IS_REPLACE_FRAGMENT";
     public static final String IS_ADD_FRAGMENT_USED = "IS_ADD_FRAGMENT_USED"; // накладывать фрагменты
     public static final String IS_BACK_IS_REMOVE_FRAGMENT = "IS_BACK_IS_REMOVE_FRAGMENT"; // назад, удаление
     public static final String IS_DELETE_FRAGMENT_BEFORE_ADD = "IS_DELETE_FRAGMENT_BEFORE_ADD"; // удаляем предыдущий фрагмент, перед тем как загрузить новый
 
     //значение
     public static boolean isBackStackUsed;
+    public static boolean isReplaceFragment;
     public static boolean isAddFragmentUsed;
     public static boolean isBackIsRemoveFragment;
     public static boolean isDeleteFragmentBeforeAdd;
--- app/src/main/java/keyone/keytwo/lesson6_dz_zametki/SettingsFragmentTwo.java
+++ app/src/main/java/keyone/keytwo/lesson6_dz_zametki/SettingsFragmentTwo.java
@@ -1,9 +1,14 @@
 package keyone.keytwo.lesson6_dz_zametki;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,10 +18,55 @@ public class SettingsFragmentTwo extends Fragment {
         return new SettingsFragmentTwo();
     }
 
+    //инициализируем кнопки
+    RadioButton radioButtonReplace;
+    RadioButton radioButtonAdd;
+    Switch switchBackStack;
+    Switch switchBackAsRemove;
+    Switch switchDeleteBeforeAdd;
+
     // внедряем макет
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_setting, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings_two, container, false);
+        initView(v);
         return v;
     }
+
+    private void initView(View v) {
+        radioButtonAdd = v.findViewById(R.id.radioButtonAdd);
+        radioButtonAdd.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Settings.isAddFragmentUsed = isChecked;
+
+                //сохранение
+
+                //вызвали настройки
+                writeSettings();
+            }
+        });
+
+        radioButtonReplace = v.findViewById(R.id.radioButtonReplace);
+        radioButtonAdd.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Settings.isReplaceFragment = isChecked;
+                //сохранение
+                //вызвали настройки
+                writeSettings();
+            }
+        });
+    }
+    private void writeSettings() {
+        SharedPreferences sharedPreferences = requireActivity()
+                .getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+        //открываем режим записи
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //записываем по кликуна кнопку
+        editor.putBoolean(Settings.IS_ADD_FRAGMENT_USED,Settings.isAddFragmentUsed);
+        editor.putBoolean(Settings.IS_REPLACE_FRAGMENT,Settings.isReplaceFragment);
+        editor.apply(); // применяем
+    }
+
 }
